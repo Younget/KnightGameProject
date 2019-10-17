863972022f68e0fde777d95df4bf34d4249324da
Younget <48969941+Younget@users.noreply.github.com>
2019-10-16 20:29:50 -0700

    I added more programming assets
    ethan

---
+++ Assets/Programming/The rest of them/ChangeLevelOnCollide.cs
@@ -0,0 +1,30 @@
+﻿/* By: Ryan Scheppler
+* Date: 10/9/19
+* Description: Add to object to cause a level change when the player touches it.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public class ChangeLevelOnCollide : MonoBehaviour
+{
+    public string NextScene = "EndScene";
+
+    public float changeDelay = 0.2f;
+
+    public UnityEvent OnHit;
+
+   
+}
+
+private void OnCollisionEnter2D(Collision2D collision)
+{
+    if (collision.gameObject.CompareTag("Player"))
+    {
+        //run any extra effects
+        OnHit.Invoke();
+        SceneManager.LoadScene(NextScene);
+    }
+}
