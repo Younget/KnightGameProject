24f89727f2bdc842a362d8711a45ccc65060dbe7
mini <mintra.elboghdadi@digipen.edu>
2019-10-17 09:50:59 -0700

    pray

---
+++ FlatPlatformer_students_final/Assets/Flat Platformer Template/Scripts/CreateEventFunction.cs
@@ -0,0 +1,21 @@
+﻿/* By: Ryan Scheppler
+ * Date: 10/9/19
+ * Description: Add to objects meant to create things on unity events
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEventFunction : MonoBehaviour
+{
+    public GameObject ObjectToMake;
+
+    //function can be run to create given object
+    public void CreateObject()
+    {
+        Instantiate(ObjectToMake, transform.position, transform.rotation);
+    }
+
+}
+
---
+++ FlatPlatformer_students_final/Assets/Flat Platformer Template/Scripts/DelayedSceneChange.cs
@@ -0,0 +1,35 @@
+﻿/* By: Ryan Scheppler
+* Date: 10/9/19
+* Description: Add this to objects you need a scene changing function for with a delay
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DelayedSceneChange : MonoBehaviour
+{
+    public float Delay = 0.0f;
+    //name of level to load
+    public string NextScene = "GameOver";
+    
+    //function will change scene after a specified delay when run
+    public void ChangeScene()
+    {
+        print("Started");
+        StartCoroutine(DelayedChange());
+    }
+
+    IEnumerator DelayedChange()
+    {
+        print("before delay");
+
+        yield return new WaitForSeconds(Delay);
+
+        print("after");
+        SceneManager.LoadScene(NextScene);
+    }
+
+}
+
+
