637e8b663e4ee5176b07f871cbb166f9a65db12e
Younget <48969941+Younget@users.noreply.github.com>
2019-10-16 21:18:01 -0700

    Programming almost if not done
    ethan

---
+++ Assets/Programming/DamageOnCollide.cs
@@ -0,0 +1,53 @@
+﻿/* By: Ryan Scheppler
+ * Date: 10/9/19
+ * Description: Add to objects meant to damage health.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class DamageOnCollide : MonoBehaviour
+{
+    int DamageAmount = 10;
+
+    bool DestroyOnCollide = false;
+    //functions to run when damage is caused
+    public UnityEvent DamageFunctions;
+    //functions to run when object is set to destroy itself
+    public UnityEvent DestroyFunctions;
+
+    
+}
+
+private void OnCollisionEnter2D(Collision2D collision)
+{
+    Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+    if (enemy != null)
+    {
+        enemy.Health -= DamageAmount;
+
+    }
+    if (DestroyOnCollide)
+    {
+
+        Destroy(gameObject);
+    }
+}
+
+private void OnTriggerEnter2D(Collider2D collision)
+{
+    Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+    if (enemy != null)
+    {
+        enemy.Health -= DamageAmount;
+
+    }
+    if (DestroyOnCollide)
+    {
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Programming/DelayedSceneChange.cs
+++ Assets/Programming/DelayedSceneChange.cs
@@ -14,3 +14,20 @@ public class DelayedSceneChange : MonoBehaviour
     public string NextScene = "GameOver";
     
 }
+
+//function will change scene after a specified delay when run
+public void ChangeScene()
+{
+    print("Started");
+    StartCoroutine(DelayedChange());
+}
+
+IEnumerator DelayedChange()
+{
+    print("before delay");
+
+    yield return new WaitForSeconds(Delay);
+
+    print("after");
+    SceneManager.LoadScene(NextScene);
+}
